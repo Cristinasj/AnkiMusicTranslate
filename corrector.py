3682ff79fb63d090885aa5a3e9c0358a5a8c39d9
C++ris <cristinasj@protonmail.com>
2022-01-29 11:39:28 +0100

    Create corrector.py

---
+++ corrector.py
@@ -0,0 +1,4 @@
+# Extract target and native
+# delete all the clozes 
+# Clean the funny B from the text and substitute with ss 
+# Create new clozes not repeating info and separating them based on " ' ! , . ; ? etc and without varing previous clozes (so I keep the card statistics) 
